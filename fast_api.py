69d643b7fdaaf4b37c4381f7633812e8d7f8bbe3
Harryl3ivers <cofe267@outlook.com>
2026-04-08 18:40:14 +0100

    added fast api for bankserver

---
+++ fast_api.py
@@ -0,0 +1,50 @@
+from fastapi import FastAPI, HTTPException
+from decimal import Decimal
+from main.account import Account
+from main.transactions import Transaction
+
+app = FastAPI()
+account_services = Account()
+transaction_services = Transaction()
+
+@app.post("/accounts/")
+def create_account(account_number: str,account_name: str, balance: float):
+    try:
+        return account_services.create_account(account_number,account_name,Decimal(balance))
+    except Exception as e:
+        raise HTTPException(status_code=400)
+
+@app.get("/accounts/{account_number}")
+def get_account(account_number: str):
+        account = transaction_services.get_account(account_number)
+        if not account:
+             raise HTTPException(status_code=404)
+        return account
+            
+@app.post("/deposit/")
+def deposit(account_number: str,amount: float):
+     try:
+          return{
+               "new_balance": transaction_services.deposit(account_number,Decimal(amount))
+          }
+     except Exception as e:
+          raise HTTPException(status_code=400)
+
+@app.post("/withdraw/")
+def withdraw(account_number: str,amount: float):
+     try:
+          return{
+               "new_balance": transaction_services.withdraw(account_number,Decimal(amount))
+          }
+     except Exception as e:
+          raise HTTPException(status_code=400)
+      
+
+@app.post("/transfer/")
+def transfer(from_account :str , to_account: str, amount: float):
+     try:
+          return transaction_services.transfer(from_account, to_account, Decimal(amount))
+     except Exception as e:
+          raise HTTPException(status_code=400)
+    
+        
